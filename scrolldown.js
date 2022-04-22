309641ada79da887dfb29aa5b32aaf8d2820e35d
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-04-22 14:36:47 +0300

    Commit
    Commit

--- scrolldown.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('a[href^="#"]').click(function() {
-    var target = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(target).offset().top
-    }, 500); //500 - scroll length (ms)
-    return false;
-  });
-});
