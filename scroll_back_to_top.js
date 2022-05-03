b6d6eb7f9c359f39129039220ce0c6dd6c710a04
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-03 16:37:21 +0300

    Commit

--- scroll_back_to_top.js
+++ scroll_back_to_top.js
@@ -3,17 +3,16 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('.scrollup').fadeIn();
-    }else{
+    } else {
       $('.scrollup').fadeOut();
     }
   });
 
-  
+
   $('.scrollup').click(function() {
     $("html, body").animate({
       scrollTop: 0
     }, 500); //scroll length (ms)
     return false;
   });
-
 });
